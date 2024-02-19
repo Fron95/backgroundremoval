d4b5bd7c1000cc50504b2d79720af7dc0c410d15
JSJ <jsj950611@naver.com>
2024-02-19 10:38:04 +0900

    수정

--- bg_remove.py
+++ bg_remove.py
@@ -3,47 +3,58 @@ from rembg import remove
 from PIL import Image
 from io import BytesIO
 import base64
+import zipfile
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Remove background from your image")
-st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
-)
+st.write("## Remove background from your images")
 st.sidebar.write("## Upload and download :gear:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-# Download the fixed image
+# Convert image to bytes for download
 def convert_image(img):
     buf = BytesIO()
     img.save(buf, format="PNG")
     byte_im = buf.getvalue()
     return byte_im
 
+# Process and display each image
+def process_images(uploaded_files):
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
+        for uploaded_file in uploaded_files:
+            if uploaded_file is not None:
+                if uploaded_file.size > MAX_FILE_SIZE:
+                    st.error("One of the uploaded files is too large. Please upload images smaller than 5MB.")
+                    return
+                else:
+                    with st.container():
+                        col1, col2 = st.columns(2)
+                        image = Image.open(uploaded_file)
+                        col1.write("Original Image :camera:")
+                        col1.image(image)
 
-def fix_image(upload):
-    image = Image.open(upload)
-    col1.write("Original Image :camera:")
-    col1.image(image)
+                        fixed_image = remove(image)
+                        col2.write("Fixed Image :wrench:")
+                        col2.image(fixed_image)
 
-    fixed = remove(image)
-    col2.write("Fixed Image :wrench:")
-    col2.image(fixed)
-    st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+                        # Add fixed image to ZIP
+                        image_bytes = convert_image(fixed_image)
+                        zip_file.writestr(f"{uploaded_file.name}_fixed.png", image_bytes)
 
-id = st.text_input("아이디를 입력해주세요")
-st.button("로그인")
+    zip_buffer.seek(0)
+    st.sidebar.download_button(
+        label="Download All Fixed Images as ZIP",
+        data=zip_buffer,
+        file_name="fixed_images.zip",
+        mime="application/zip"
+    )
 
-if (id=="jy") &  :
-    col1, col2 = st.columns(2)
-    my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+id = st.text_input("Please enter your ID")
+login = st.button("Login")
 
-    if my_upload is not None:
-        if my_upload.size > MAX_FILE_SIZE:
-            st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
-        else:
-            fix_image(upload=my_upload)
-    else:
-        fix_image("./zebra.jpg")
+if id == "jy" and login:
+    uploaded_files = st.sidebar.file_uploader("Upload images", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
+    if uploaded_files:
+        process_images(uploaded_files)
